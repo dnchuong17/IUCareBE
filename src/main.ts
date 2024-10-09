b74b749369099fd54630ba4b455f09f697d013f3
dnchuong17 <chuongdo172@gmail.com>
2024-10-09 16:46:05 +0700

    insert cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import * as process from "process";
-import {AuthGuard} from "./auth/auth.guard";
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors());
   await app.listen(process.env.PORT_SERVER);
 }
 bootstrap();
