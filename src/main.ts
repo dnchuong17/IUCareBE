0d8f0b1834abdf2c9f8382512f3d19a0a69c38e1
dnchuong17 <chuongdo172@gmail.com>
2024-10-10 16:26:57 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import * as process from "process";
 import * as cors from 'cors';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cors());
   await app.listen(process.env.PORT_SERVER);
