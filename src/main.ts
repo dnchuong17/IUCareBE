b2a0788a90eeec8c4f5b889ea7fd8f11e6fc7295
dnchuong17 <chuongdo172@gmail.com>
2024-10-12 01:28:09 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import {NestExpressApplication} from "@nestjs/platform-express";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cors());
+  app.enableCors(cors);
   await app.listen(process.env.PORT_SERVER);
 }
 bootstrap();
