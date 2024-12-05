b1f7ff25619d578e7603d66ff3c416a80331d90b
dnchuong17 <chuongdo172@gmail.com>
2024-12-06 00:30:11 +0700

    fix appointment

--- src/common/utils/date.utils.ts
+++ src/common/utils/date.utils.ts
@@ -1,8 +1,9 @@
 export class DateUtils {
-    formatDate(yourDate: Date) {
-        const offset = yourDate.getTimezoneOffset();
-        yourDate = new Date(yourDate.getTime() - (offset * 60 * 1000))
-        return yourDate.toISOString();
+    formatDate(date: Date): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
     }
 
     formatStringToDate(dbTime: string) {
@@ -11,5 +12,9 @@ export class DateUtils {
         const time = date.getTime();
         return new Date(time + offset * 60 * 1000);
     }
+
+
+
+
 }
 
