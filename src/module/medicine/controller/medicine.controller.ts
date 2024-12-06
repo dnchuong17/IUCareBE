9df0de16b3e5d858453f45fd014a3187e58a976e
dnchuong17 <chuongdo172@gmail.com>
2024-12-07 06:12:55 +0700

    fix medicines

--- src/module/medicine/controller/medicine.controller.ts
+++ src/module/medicine/controller/medicine.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, Query} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post, Query} from "@nestjs/common";
 import {MedicineService} from "../service/medicine.service";
 import {Public} from "../../../auth/decorator/public.decorator";
 
@@ -12,5 +12,12 @@ export class MedicineController {
             return [];
         }
         return this.medicineService.searchMedicine(medicine_name);
+    }
+
+    @Post('/add')
+    async addMedicines(
+        @Body('medicalRecordId') medicalRecordId: number,
+        @Body('medicineIds') medicineIds: number[]) {
+        return this.medicineService.addMedicinesToRecord(medicalRecordId, medicineIds);
     }
 }
--- src/module/medicine/service/medicine.service.ts
+++ src/module/medicine/service/medicine.service.ts
@@ -9,6 +9,25 @@ export class MedicineService {
     async searchMedicine(medicineName: string) {
         const query = 'SELECT name_medicine FROM medicine WHERE name_medicine ILIKE $1';
         const result = await this.dataSource.query(query, [`%${medicineName}%`]);
-        return result.map((item) => item.name_medicine);
+        return result.map((item) => ({
+            id: item.medicine_id,
+            name: item.name_medicine,
+        }));
     }
+
+    async addMedicinesToRecord(medicalRecordId: number, medicineIds: number[]) {
+        const insertMedicinesQuery = `
+        INSERT INTO medicine_record (medical_record_id, medicine_id)
+        VALUES ($1, $2)
+    `;
+
+        for (const medicineId of medicineIds) {
+            await this.dataSource.query(insertMedicinesQuery, [medicalRecordId, medicineId]);
+        }
+
+        return {
+            message: `Medicines added to medical record ID ${medicalRecordId} successfully.`,
+        };
+    }
+
 }
