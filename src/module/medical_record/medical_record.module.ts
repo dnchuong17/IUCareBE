c4064328f12a58e74a2a5c7f75a4372a9785ac27
dnchuong17 <chuongdo172@gmail.com>
2024-12-09 14:54:14 +0700

    get record detail

--- src/module/medical_record/medical_record.module.ts
+++ src/module/medical_record/medical_record.module.ts
@@ -4,10 +4,11 @@ import {MedicalRecordService} from "./service/medical_record.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Medical_recordEntity} from "./entity/medical_record.entity";
 import {DateUtils} from "../../common/utils/date.utils";
+import {RedisHelper} from "../redis/redis.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Medical_recordEntity])],
     controllers: [MedicalRecordController],
-    providers: [MedicalRecordService, DateUtils],
+    providers: [MedicalRecordService, DateUtils, RedisHelper],
 })
 export class MedicalRecordModule {}
